4861224866ff392b634c22171cf6b2be2d2c1a68
OU1122 <152392720+OU1122@users.noreply.github.com>
2024-10-03 22:08:43 +0100

    search result logic updated

--- src/components/searchResult.tsx
+++ src/components/searchResult.tsx
@@ -4,25 +4,33 @@ import { RecommendedCard } from "./recommendedCard";
 export const SearchResult: React.FC<SearchResultsProps> = ({
 	movieData,
 	searchQuery,
-	setSearchQuery,
 }) => {
-	const recommendedMovies = movieData.filter((movie) =>
+	const searchResults = movieData.filter((movie) =>
 		movie.title.toLowerCase().includes(searchQuery?.toLowerCase() || "")
 	);
-	console.log(recommendedMovies);
+
 	return (
 		<div className="w-full max-w-[1240px] pb-2">
-			<h2 className="text-White text-[32px] font-light mb-6">
-				Search results:
-			</h2>
-			<div className="grid grid-cols-4  gap-[40px]">
-				{recommendedMovies.map((movie, index) => (
-					<RecommendedCard
-						key={index}
-						movie={movie}
-					/>
-				))}
-			</div>
+			{searchResults.length === 0 ? (
+				<h2 className="text-White text-[32px] font-light mb-6">
+					No results found for your search term. Please try again.
+				</h2>
+			) : (
+				<>
+					<h2 className="text-White text-[32px] font-light mb-6">
+						Found {searchResults.length} results for search term:{" "}
+						{searchQuery}
+					</h2>
+					<div className="grid grid-cols-4  gap-[40px]">
+						{searchResults.map((movie, index) => (
+							<RecommendedCard
+								key={index}
+								movie={movie}
+							/>
+						))}
+					</div>{" "}
+				</>
+			)}
 		</div>
 	);
 };
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export const SideBar: React.FC = () => {
 	return (
 		<nav className="bg-SemiDarkBlue w-[96px] max-h-[960px] h-[90vh] rounded-[10px] mb-4  border flex flex-col justify-between py-5 ml-5 shrink-0">
@@ -9,26 +11,36 @@ export const SideBar: React.FC = () => {
 					/>
 				</div>
 				<div className="flex flex-col items-center justify-center gap-6">
-					<img
-						src="../src/assets/icon-nav-home.svg"
-						alt="home"
-						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
-					/>
-					<img
-						src="../src/assets/icon-nav-movies.svg"
-						alt="movies"
-						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
-					/>
-					<img
-						src="../src/assets/icon-nav-tv-series.svg"
-						alt="tv-series"
-						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
-					/>
-					<img
-						src="../src/assets/icon-nav-bookmark.svg"
-						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
-						alt="bookmark"
-					/>
+					<NavLink
+						to={"/"}
+						className={({ isActive }) => (isActive ? "active" : "")}>
+						<img
+							src="../src/assets/icon-nav-home.svg"
+							alt="home"
+							className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
+						/>
+					</NavLink>
+					<NavLink to={"/"}>
+						<img
+							src="../src/assets/icon-nav-movies.svg"
+							alt="movies"
+							className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
+						/>
+					</NavLink>
+					<NavLink to={"/"}>
+						<img
+							src="../src/assets/icon-nav-tv-series.svg"
+							alt="tv-series"
+							className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
+						/>
+					</NavLink>
+					<NavLink to={"/"}>
+						<img
+							src="../src/assets/icon-nav-bookmark.svg"
+							className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
+							alt="bookmark"
+						/>
+					</NavLink>
 				</div>
 			</div>
 			<div className="flex items-center justify-center py-4">
