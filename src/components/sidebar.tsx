113b06faccbbd7476cf1538e9e35597c20c46f7b
Randomnickname12 <eliquinoxltd@gmail.com>
2024-10-03 15:45:44 +0200

    css adjustments

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -11,23 +11,27 @@ export const SideBar: React.FC = () => {
 				<div className="flex flex-col items-center justify-center gap-6">
 					<img
 						src="../src/assets/icon-nav-home.svg"
-						alt=""
+						alt="home"
+						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
 					/>
 					<img
 						src="../src/assets/icon-nav-movies.svg"
-						alt=""
+						alt="movies"
+						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
 					/>
 					<img
 						src="../src/assets/icon-nav-tv-series.svg"
-						alt=""
+						alt="tv-series"
+						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
 					/>
 					<img
 						src="../src/assets/icon-nav-bookmark.svg"
-						alt=""
+						className="hover:cursor-pointer hover:brightness-[1.2] hover:saturate-[300%] hover:hue-rotate-[125deg] hover:contrast-100 filter"
+						alt="bookmark"
 					/>
 				</div>
 			</div>
-			<div className="flex items-center justify-center">
+			<div className="flex items-center justify-center py-4">
 				<img
 					className="w-10 h-10 border-White border rounded-full"
 					src="../src/assets/image-avatar.png"
