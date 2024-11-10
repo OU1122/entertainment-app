9bd7b4bf7f60f59ad67b009ce36dd9ebb51868c6
OU1122 <152392720+OU1122@users.noreply.github.com>
2024-11-10 15:41:51 +0100

    Update recommendedCard.tsx

--- src/components/recommendedCard.tsx
+++ src/components/recommendedCard.tsx
@@ -41,24 +41,27 @@ export const RecommendedCard: React.FC<RecommendedCardProps> = ({ movie }) => {
 					src={movie.thumbnail.regular.large}
 					className="object-cover rounded-2xl w-full h-full"
 					alt={movie.title}></img>
-
-				<div
-					onClick={setBookmark}
-					className={`absolute top-4 right-4 h-8 w-8 rounded-full bg-DarkBlue/50 hover:bg-White flex items-center justify-center group transition-all ease-in z-10`}>
-					{isFavourite ? (
+				{isFavourite ? (
+					<div
+						onClick={setBookmark}
+						className={`absolute top-4 right-4 h-8 w-8 rounded-full bg-White/70 hover:bg-White flex items-center justify-center group transition-all ease-in z-10`}>
 						<img
 							src="./assets/icon-bookmark-empty.svg"
 							alt="bookmark"
-							className={`transition-all ease-in`}
+							className={` filter brightness-100 invert transition-all ease-in`}
 						/>
-					) : (
+					</div>
+				) : (
+					<div
+						onClick={setBookmark}
+						className={`absolute top-4 right-4 h-8 w-8 rounded-full bg-DarkBlue/50 hover:bg-White flex items-center justify-center group transition-all ease-in z-10`}>
 						<img
 							src="./assets/icon-bookmark-empty.svg"
 							alt="bookmark"
 							className={` filter group-hover:brightness-100 group-hover:invert transition-all ease-in`}
 						/>
-					)}
-				</div>
+					</div>
+				)}
 				{/* Hover EFfect Overlay*/}
 				<div className="absolute inset-0 bg-DarkBlue/50 opacity-0 group-hover/play:opacity-100 rounded-2xl cursor-pointer transition-opacity duration-200">
 					<div className="absolute top-1/2 left-1/2 flex items-center justify-center gap-4 -translate-x-1/2 -translate-y-1/2 bg-White/25 px-2 py-2 rounded-full">
